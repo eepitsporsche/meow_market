99d38743edd939806302e95786c706d703c8e7c6
Jenina <jeninadelacruz521@yahoo.com>
2024-07-16 20:25:08 -0700

    added stripe APIkeys

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
@@ -6,7 +8,6 @@ const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
